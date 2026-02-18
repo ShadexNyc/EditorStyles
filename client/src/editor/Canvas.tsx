1dcb35fbc11265ed7fc3b34b22ab3ea39dff2d28
ShadexNyc <sereja.nyc@gmail.com>
2026-02-18 16:47:59 +0300

    Обновить текст задания в плашке над листом

--- client/src/editor/Canvas.tsx
+++ client/src/editor/Canvas.tsx
@@ -10,6 +10,7 @@ export function Canvas({ children }: CanvasProps) {
       <section className="task-block" aria-label="Задание">
         <p className="task-block-title">Задание:</p>
         <ol className="task-block-list">
+          <li>Включите в онлайн редакторе режим "Предложения изменений".</li>
           <li>Найдите в тексте утверждение "Они развивают мышление и расширяют кругозор" и предложите свой вариант изменения.</li>
         </ol>
       </section>
